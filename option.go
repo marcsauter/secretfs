4216c54b6baceeaa62424384ae9b1dea162edd19
Marc Sauter <marc.sauter@postfinance.ch>
2022-04-19 09:50:36 +0200

    chore: complete rename

--- option.go
+++ option.go
@@ -5,12 +5,12 @@ import (
 )
 
 // Option represents a functional Option
-type Option func(*secretFs)
+type Option func(*sekretsFs)
 
 // WithSecretPrefix configures a custom secret prefix
 // TODO: does this belong here?
 func WithSecretPrefix(x string) Option {
-	return func(s *secretFs) {
+	return func(s *sekretsFs) {
 		s.prefix = x
 	}
 }
@@ -18,14 +18,14 @@ func WithSecretPrefix(x string) Option {
 // WithSecretSuffix configures a custom secret prefix
 // TODO: does this belong here?
 func WithSecretSuffix(x string) Option {
-	return func(s *secretFs) {
+	return func(s *sekretsFs) {
 		s.suffix = x
 	}
 }
 
 // WithLogger configures a logger
 func WithLogger(l *zap.SugaredLogger) Option {
-	return func(s *secretFs) {
+	return func(s *sekretsFs) {
 		s.l = l
 	}
 }
